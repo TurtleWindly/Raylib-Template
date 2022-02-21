5457dd195885202cb037c4b5efa79194751154fa
TurtleWindly <quachquangminh2006@gmail.com>
2022-02-21 14:25:20 +0700

    turn off -Wno-enum-compare to work with raygui

--- Tasks/build.cmd
+++ Tasks/build.cmd
@@ -1,1 +1,1 @@
-gcc main.cpp -o .\game\game.exe -O1 -Wall -std=c++17 -Wno-missing-braces -I include/ -L lib\ -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc main.cpp -o .\game\game.exe -O1 -Wall -std=c++17 -Wno-enum-compare -Wno-missing-braces -I include/ -L lib\ -lraylib -lopengl32 -lgdi32 -lwinmm
